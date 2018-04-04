f084c6a02c06d07982eaf62157efc137d101dd04
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-04-04 20:23:57 +0200

    Add js function to show figures

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+// Assignment of figures array
+const figure = document.getElementsByClassName('circle');
+const figures = [...figure];
 
+for (let i = 1; i <= figures.lenght; i++){
+  figures[i].addEventListener('click', function(){
+    this.classList.toggle('clicked');
+  })
+}
